2d04d25ba8004474081c36d00cc764819353f70e
Valentin Lavrinenko <valentin@people.ai>
2020-08-26 14:58:21 +0300

    move to vim 8

--- config.fish
+++ config.fish
@@ -1,8 +1,8 @@
 # export PATH="/usr/local/opt/python/libexec/bin:$PATH"
-eval (python -m virtualfish auto_activation global_requirements)
+# eval (python3 -m virtualfish auto_activation global_requirements)
 eval (docker-machine env default)
 
-alias vim (which nvim)
+# alias vim (which nvim)
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
